72a3be7b14a82d5ed8474bf24e93bc07043f811e
Jedid-Ahmed <jedidahmed88@gmail.com>
2022-06-30 22:06:39 +0300

    Commits

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    for name in dir(hidden_4):
-        if name.startswith('__'):
-            continue
-        print("{:s}".format(name))
+if __name__ == '__main__':
+    from variable_load_5 import a
+    print('{}'.format(a))
