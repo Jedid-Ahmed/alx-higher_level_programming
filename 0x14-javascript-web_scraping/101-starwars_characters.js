a8e123eb5161672447f8845af968e9b977978dec
Jedid-Ahmed <44965714+Jedid-Ahmed@users.noreply.github.com>
2022-11-15 22:05:54 +0300

    fix error

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
+const args = process.argv;
+const url = 'https://swapi-api.hbtn.io/api/films/' + args[2];
+request(url, async function (error, response, body) {
+  if (error === null) printer(JSON.parse(body).characters, 0);
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+function printer (characters, i) {
+  request(characters[i], function (error, response, body) {
+    if (error === null) {
+      console.log(JSON.parse(response.body).name);
+      if (i + 1 < characters.length) printer(characters, i + 1);
     }
   });
 }
